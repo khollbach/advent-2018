c8f452420aca3bee69819d109d6c87bdfbff57b3
Kevan Hollbach <khollbach@gmail.com>
2022-01-18 20:44:58 -0500

    Copy project structure from another year

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+
+    let prog_name = args.next().unwrap();
+    let args: Vec<_> = args.collect();
+
+    let usage = || -> ! {
+        eprintln!();
+        eprintln!("Usage: `{prog_name} <num>`");
+        eprintln!("where <num> is a number from 1 through 25.");
+        process::exit(1);
+    };
+
+    if args.len() != 1 {
+        eprintln!("Expected 1 argument, got {}.", args.len());
+        usage();
+    }
+
+    let day: u32 = match args[0].parse() {
+        Ok(n) if 1 <= n && n <= 25 => {
+            n
+        }
+        _ => {
+            eprintln!("Expected a number from 1 through 25, got: {}", args[0]);
+            usage();
+        }
+    };
+
+    advent_2018::solve(day);
 }
