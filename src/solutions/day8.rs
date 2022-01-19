4154bd79cfa1e8e5b7a22f6ff7d2abd4719ad606
Kevan Hollbach <khollbach@gmail.com>
2022-01-18 21:13:28 -0500

    8-1

--- src/solutions/day8.rs
+++ src/solutions/day8.rs
@@ -1,3 +1,44 @@
+use std::{io, iter};
+use std::io::BufRead;
+use itertools::Itertools;
+
+fn read_input(input: impl BufRead) -> Vec<usize> {
+    let (line,) = input.lines().map(Result::unwrap).collect_tuple().unwrap();
+
+    line.split_whitespace().map(|n| {
+        n.parse().unwrap()
+    }).collect()
+}
+
 pub fn main() {
-    dbg!(8);
+    let nums = read_input(io::stdin().lock());
+
+    let tree = parse_tree(&mut nums.into_iter());
+    println!("{}", tree.metadata_sum());
+}
+
+struct Tree {
+    children: Vec<Tree>,
+    metadata: Vec<usize>,
+}
+
+impl Tree {
+    fn metadata_sum(&self) -> usize {
+        let self_sum: usize = self.metadata.iter().sum();
+        let children_sum: usize = self.children.iter().map(Tree::metadata_sum).sum();
+        self_sum + children_sum
+    }
+}
+
+fn parse_tree(nums: &mut impl Iterator<Item=usize>) -> Tree {
+    let num_children = nums.next().unwrap();
+    let num_metadata = nums.next().unwrap();
+
+    let children = iter::repeat_with(|| {
+        parse_tree(nums)
+    }).take(num_children).collect();
+
+    let metadata = nums.take(num_metadata).collect();
+
+    Tree { children, metadata }
 }
